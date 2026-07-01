7ad6f3f67aedbe241b3ec634ca2d4f99ed4b6b04
Cʀʏᴘᴛɪᴄ <117926278+UnIntegrated-AI@users.noreply.github.com>
2026-07-02 01:32:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,52 @@
 import customtkinter as ctk
+from screeninfo import get_monitors
 
+monitor = get_monitors()[0]
+
+HEIGHT = monitor.height
+WIDTH = monitor.width
 
 class App(ctk.CTk):
     def __init__(self):
         super().__init__()
         self.title("Buckshot")
-        self.geometry("1000x600")
+        self.geometry(f"{WIDTH // 2}x{HEIGHT // 2}")
         self.resizable(False,False)
         self.grid_rowconfigure(0,weight=1)
         self.grid_columnconfigure(0,weight=1)
+        login_frame = LoginFrame(self)
+        login_frame.grid(row=0, column=0)
+        
+class LoginFrame(ctk.CTkFrame):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        app_width = WIDTH // 2
+        app_height = HEIGHT // 2
+
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(0, weight=1)
+        
+        self.entry_frame = ctk.CTkFrame(
+            self,
+            fg_color="black",
+            width=(app_width // 2),
+            height=(app_height // 2)
+        )
+        self.entry_frame.grid(row=0, column=0, sticky="nsew")
+
+        self.entry_frame.grid_propagate(False)
+
+        self.entry_frame.grid_rowconfigure(0,weight=1)
+        self.entry_frame.grid_columnconfigure(0, weight=1)
+
+        self.header_lbl = ctk.CTkLabel(
+            self.entry_frame,
+            text="Buckshot",
+            font=("Segoe UI", 30, "bold"),
+        )
+        self.header_lbl.grid(row=0, column=0, sticky="nswe")
+
         
 
 app = App()
